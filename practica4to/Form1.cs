7f2aad35b809c19d8c7304a40cbe845476f259ca
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2022-05-18 23:10:30 -0400

    Add files via upload

---
+++ practica4to/Form1.cs
@@ -0,0 +1,36 @@
+namespace practica4to
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Hello World";
+            label5.Text = "Felicidades, hiciste tu primer codigo.";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
